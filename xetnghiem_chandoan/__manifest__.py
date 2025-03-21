4422e6b9dfa94c98e80b3a4abd481d1a60f767cf
Danh <danh123098@gmail.com>
2025-03-21 15:27:24 +0700

    Module xet nghiem chan doan

---
+++ xetnghiem_chandoan/__manifest__.py
@@ -0,0 +1,22 @@
+{
+    'name': "Quan ly xet nghiem va chan doan",
+    'version': '1.1',
+    'depends': ['base'],
+    'author': "Your Name",
+    'category': 'Healthcare',
+    'summary': "A module for managing hospital operations, patients, and doctors",
+    'description': """
+        This module helps in managing hospital records, including patients, doctors, and appointments.
+    """,
+    'data': [
+        'views/chandoan_action.xml',
+        'views/chandoan_menu.xml',
+        'views/xetnghiem.xml',
+        'security/ir.model.access.csv',
+    ],
+    'demo': [
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
---
+++ xetnghiem_chandoan/models/xetnghiem.py
@@ -0,0 +1,18 @@
+﻿from odoo import api, fields, models
+
+class Xetnghiem(models.Model):
+    _name = "benhvien.xetnghiem"
+    _description = "Xet Nghiem Management"
+
+    ma_xet_nghiem = fields.Char(string="Mã xét nghiệm", required=True)
+    ten_loai_xet_nghiem = fields.Char(string="Tên loại xét nghiệm", required=True)
+    ma_benh_an = fields.Char(string="Ma benh an", required=True)
+    ten_bac_si = fields.Char(string="Ten bac si", required=True)
+    name = fields.Char(string="Tên mẫu xét nghiệm", required=True)
+    thoi_gian_bat_dau = fields.Datetime(string="Thời gian bắt đầu", required=True)
+    thoi_gian_ket_thuc = fields.Datetime(string="Thời gian kết thúc")
+    trang_thai = fields.Selection([
+        ('dang_lay_mau', 'Đang lấy mẫu'),
+        ('dang_xet_nghiem', 'Đang xét nghiệm')
+    ], default='dang_lay_mau', string='Trạng thái')
+    ket_qua = fields.Text(string="Kết quả")
