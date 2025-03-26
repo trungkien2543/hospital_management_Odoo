59c0d253c8abac80ddd2173046a4895eea13705f
duyKhanh026 <trankhanhduy12a12@gmail.com>
2025-03-26 10:05:34 +0700

    fixed

--- nhansu_tienluong/models/my_experience.py
+++ nhansu_tienluong/models/my_experience.py
@@ -4,9 +4,8 @@ class MyExperience(models.Model):
     _name = 'benhvien.nhansu.kinhnghiem'
     _description = 'Experience'
 
-    experience_code = fields.Integer(string="Mã Kinh nghiệm", required=True)  # int
     title = fields.Char(string="Tiêu đề", required=True)  # nvarchar
     description = fields.Text(string="Mô tả kinh nghiệm")  # nvarchar
     start_date = fields.Datetime(string="Ngày bắt đầu")  # datetime
     end_date = fields.Datetime(string="Ngày kết thúc")  # datetime
-    employee_code = fields.Many2one('benhvien.nhansu', string="Nhân sự", required=True, ondelete='cascade')
+    employee_code = fields.Many2one('benhvien.nhansu', string="Nhân sự", ondelete='cascade')
