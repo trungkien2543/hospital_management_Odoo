16d0091100c8b32da23b00bea0343b82d0c73459
Danh <danh123098@gmail.com>
2025-03-29 09:20:44 +0700

    Ket noi giuong benh, xet nghiem, hinh anh vao benh án

--- benh_vien/models/quan_ly_xetnghiem_chandoan/xetnghiem.py
+++ benh_vien/models/quan_ly_xetnghiem_chandoan/xetnghiem.py
@@ -6,8 +6,8 @@ class Xetnghiem(models.Model):
 
     ma_xet_nghiem = fields.Char(string="Mã xét nghiệm", required=True, copy=False, readonly=True, default="Không cần nhập")
     ten_loai_xet_nghiem = fields.Many2one('benhvien.loaichandoan', string="Loại xét nghiệm", required=True)
-    ma_benh_an = fields.Char(string="Mã bệnh án", required=True)
-    ten_bac_si = fields.Char(string="Ten bác sĩ", required=True)
+    ma_benh_an = fields.Many2one("benhvien.hosobenhan", string="Mã Bệnh án", required=True)
+    ten_bac_si = fields.Many2one("benhvien.nhansu", string="Bác Sĩ Phụ Trách", required=True)
     name = fields.Char(string="Tên mẫu xét nghiệm", required=True)
     thoi_gian_bat_dau = fields.Datetime(string="Thời gian bắt đầu", required=True)
     thoi_gian_ket_thuc = fields.Datetime(string="Thời gian kết thúc")
